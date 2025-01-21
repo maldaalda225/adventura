4cfa16b509f1c93107132b0031aaf8b2ab82d0b5
maldaalda225 <malik@soje.cz>
2025-01-21 11:03:41 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+# Globální proměnné
+zivoty = 100
+sila = 5
+
+def uvod():
+    print("\nVítej, dobrodruhu!\n")
+    print("Stojíš na rozcestí. Máš čtyři možnosti:")
+    print("1. Vstoupit do temného lesa.")
+    print("2. Vyrazit na starý hrad.")
+    print("3. Jít k tichému jezeru.")
+    print("4. Postavit se tajemnému bossovi.")
